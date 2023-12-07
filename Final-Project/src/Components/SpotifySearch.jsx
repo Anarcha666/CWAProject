f291577322988ef5072c59041d663369f1b9508b
Vladimir <le0679993@crc-lennox.qc.ca>
2023-12-07 14:21:31 -0500

    spotify search

--- Final-Project/src/Components/SpotifySearch.jsx
+++ Final-Project/src/Components/SpotifySearch.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 const SpotifySearch = ({ goBack }) => {
   return (
-    <div>
+    <div className="container">
       <h2>Spotify Search</h2>
-      <button onClick={goBack}>Back to App</button>
+      <div className="row">
+        <div className="col-8">
+          <div className="input-group mb-3">
+            <input type="search" className="form-control" placeholder="Search Spotify" />
+          </div>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-primary" onClick={goBack}>Back to App</button>
+        </div>
+      </div>
     </div>
   );
 };
