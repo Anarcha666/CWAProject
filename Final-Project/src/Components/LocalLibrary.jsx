392920f7311106d037c7718a1b13e5ec6828c064
Vladimir <le0679993@crc-lennox.qc.ca>
2023-12-11 14:46:43 -0500

    fixed the audio and localstorage

--- Final-Project/src/Components/LocalLibrary.jsx
+++ Final-Project/src/Components/LocalLibrary.jsx
@@ -1,40 +1,50 @@
 // LocalLibrary.jsx
-import React, { useState } from 'react';
-import Track from './Track';
-import trackData from './trackData';
-import H5AudioPlayer from 'react-h5-audio-player';
+import { useState } from "react";
+import Track from "./Track";
+import trackData from "./trackData";
+import H5AudioPlayer from "react-h5-audio-player";
 
 const LocalLibrary = () => {
 	const [tracks, setTracks] = useState(trackData);
-	
 	const [currentTrack, setCurrentTrack] = useState(tracks[0]);
 
-	const handlePlay = (id) => {
-		const selectedTrack = tracks.find(track => track.id === id);
-		setCurrentTrack(selectedTrack);
-		setTracks((prevTracks) =>
-			prevTracks.map((track) =>
-				track.id === id ? { ...track, listens: track.listens + 1 } : track
-			)
-		);
-		// Play the audio for the selected track
+	function modifyTrackListens(id) {
+		const storedValue = localStorage.getItem(`trackListens-${id}`);
+		const parsedValue = parseInt(storedValue, 10);
+		const trackListens = isNaN(parsedValue) ? 0 : parsedValue;
+		const updatedValue = trackListens + 1;
+		localStorage.setItem(`trackListens-${id}`, updatedValue.toString());
+	}
 
+	const handlePlay = (id, increment = false) => {
+		const selectedTrack = tracks.find((track) => track.id === id);
+		if (currentTrack.id === selectedTrack.id) {
+			setCurrentTrack(selectedTrack); // force a re-render
+		}
+		setCurrentTrack(selectedTrack);
+		if (increment) {
+			modifyTrackListens(id);
+		}
 	};
 
 	return (
-		<div className='text-center'>
+		<div className="text-center">
 			<div className="container">
 				<h2>Local Library</h2>
 				<div className="row">
 					<div className="col">
 						{tracks.map((track) => (
-							<Track key={track.id} track={track} onPlay={() => handlePlay(track.id)} />
+							<Track key={track.id} track={track} onPlay={handlePlay} />
 						))}
 					</div>
 				</div>
-				{currentTrack && <H5AudioPlayer src={currentTrack.audioUrl} />}
+				{currentTrack && (
+					<H5AudioPlayer
+						src={currentTrack.audioUrl}
+						onEnded={(e) => handlePlay(currentTrack.id, true)}
+					/>
+				)}
 			</div>
-
 		</div>
 	);
 };
--- Final-Project/src/Components/Track.jsx
+++ Final-Project/src/Components/Track.jsx
@@ -1,28 +1,19 @@
 // Track.js
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
 
 const Track = ({ track, onPlay }) => {
-  const [canPlay, setCanPlay] = useState(false);
-  const storedValue = localStorage.getItem('trackListens');
-  const parsedValue = parseInt(storedValue, 10);
-  const trackListens = isNaN(parsedValue) ? 0 : parsedValue;
+  const [listens, setListens] = useState(0);
 
-  const [trackListensState, setTrackListensState] = useState(trackListens);
-
-
-  function modifyTrackListens() {
-
-    setTrackListensState(prevTrackListensState => {
-      const updatedValue = prevTrackListensState + 1;
-      console.log(updatedValue);
-      localStorage.setItem('trackListens', updatedValue.toString());
-      return updatedValue;
-    });
-  }
+  useEffect(() => {
+    const storedValue = localStorage.getItem(`trackListens-${track.id}`);
+    const parsedValue = parseInt(storedValue, 10);
+    const trackListens = isNaN(parsedValue) ? 0 : parsedValue;
+    setListens(trackListens);
+  }, [track.id]);
 
   return (
     <div className="container">
@@ -32,23 +23,15 @@ const Track = ({ track, onPlay }) => {
           <div className="col">Artist: {track.artist}</div>
           <div className="col">{track.duration}</div>
         </div>
-        { <button onClick={() => {
-        onPlay(track.id);
-        setCanPlay(!canPlay); // Toggle canPlay state
-      }}>
-         Play
-      </button>}
-        <div>Listens: {trackListensState}</div>
+        {<button onClick={() => {
+          onPlay(track.id);
 
-        <AudioPlayer
-          autoPlay={canPlay}
-          src={track.audioUrl}
-          onPlay={(e) => console.log('onPlay')}
-          onPause={(e) => console.log('onPause')}
-          onEnded={(e) => modifyTrackListens()}
-          style={{ visibility: 'hidden', pointerEvents: 'none' }}
-        // other props here
-        />
+        }}>
+          Play
+        </button>}
+        <div>Listens: {listens}</div>
+
+
       </div>
     </div>
 
