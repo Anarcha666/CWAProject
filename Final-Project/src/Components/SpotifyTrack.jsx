fd572a22d843b35035f0c250afc4c9bf7b1de8a3
Mx.Chetty <Le1727424@crcmail.net>
2023-12-12 12:07:19 -0500

    made buttons but needs proper spacing

--- Final-Project/src/Components/SpotifyTrack.jsx
+++ Final-Project/src/Components/SpotifyTrack.jsx
@@ -5,14 +5,18 @@ function SpotifyTrack(track) {
     console.log(track)
   return (
     <div>
-<div className="container">
+<div className="container" >
     <div className="row">
     <div className="col">Title: {track.track.name}</div>
     <div className="col">Artist: {track.artists}</div>
     <div className="col">{track.duration}</div>
 
-   <div className="col">Preview </div>
-   <div className="col">Open In Spotify </div>
+   <div className="col">
+    <button>Preview</button> 
+    </div>
+   <div className="col">
+    <button>Open in Spotify</button> 
+    </div>
    </div>
 </div>
 
